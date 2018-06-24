d205dbdd27ae7676c731f588809b0c4343adfa37
brislov <brislov@users.noreply.github.com>
2018-06-24 21:04:07 +0200

    Now it actually works!

--- search_dictionaries.py
+++ search_dictionaries.py
@@ -1,57 +1,41 @@
 import time
 
 
-def find(query, dictionary, path=''):
-    """ Find key(query) in dictionary.
-
-    :param query: Key to search for
-    :param dictionary: Dictionary to search in
-    :param path: Used for saving path during recursion
-    :return: path, key, value
-    """
+def find(query, dictionary):
+    """ Find key(query) in dictionary. Can handle nested dictionaries and returns a generator list of tuples with
+    each tuple consisting of path, key and value for each find. """
     for key, value in dictionary.items():
         if key == query:
-            yield path + '{', key, value
-
+            path = key
+            yield (path, key, value)
         elif isinstance(value, dict):
-            path += '{\'' + key + '\': '
-            for result in find(query, value, path):   # recursion
-                yield result
+            for p, k, v in find(query, value):  # recursion
+                yield (key + ' -> ' + p, k, v)
 
 
-def run(*dictionaries):
-    """ A loop for searching keys in passed through dictionaries.
-
-    :param dictionaries: Dictionaries to pass through
-    :return: None
-    """
+def search_dictionaries(*dictionaries):
+    """ Loop for searching keys in passed through dictionaries. """
     while True:
-        query = input('Search key:')
+        query = input('Search key: ')
 
-        # Enter "quit" to end loop
+        # Type "quit" to end loop
         if query.startswith('quit'):
             break
 
-        start_time = time.clock()
-
         found = False
         times = 0
+        str_out = ''
+        start_time = time.clock()
+
         for dictionary in dictionaries:
             for path, key, value in find(query, dictionary):
+                found = True
+                times += 1
+                str_out += '\t{}{}: {}\n'.format(path[:-len(key)], key, value)
 
-                if key == query:
-                    found = True
-                    times += 1
+        # Search time in ms
+        elapsed_time = (time.clock() - start_time) * 10**6
 
-                # Output dictionary structure
-                output = '{}\'{}\': {}'.format(path, key, value)
-                for char in output:
-                    if char == '{':
-                        output += '}'
+        str_out += '\n\tFound = {}, Times = {} \tin {} ms\n'.format(found, times, elapsed_time)
 
-                print(output)
-
-        # Search time in ns
-        duration = (time.clock() - start_time) * 1000000
-
-        print('Found = {}, Times = {} in {} ns\n'.format(found, times, duration))
+        print(str_out)
